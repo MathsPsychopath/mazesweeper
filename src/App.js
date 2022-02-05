2e5cb1dffa9394a9e1a613be504a0721d0f32f26
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-02-05 01:08:02 +0000

    set up react router for use. https://css-tricks.com/the-hooks-of-react-router/

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import {
   increaseCounter,
   decreaseCounter,
 } from "./redux/Counter/counter.actions";
+import {
+  Switch,
+  useRouteMatch,
+  Route
+} from "react-router-dom";
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch> 
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 
 import {Provider} from "react-redux";
 import store from "./redux/store";
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <HashRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </HashRouter>,
   document.getElementById('root')
 );
 
