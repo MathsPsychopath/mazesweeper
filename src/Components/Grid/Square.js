7ac00c92c2c86166193ea8fd169a5e424fdfc71b
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-02-10 18:09:39 +0000

    added Square Component and passing tests

--- src/Components/Grid/Square.js
+++ src/Components/Grid/Square.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import "./square.css";
+
+const COLOR_1 = "bg-white";
+const COLOR_2 = "bg-orange-500";
 
 export default function Square() {
-  return <div></div>;
+  const [color, setColor] = useState(COLOR_1);
+  const classes = [color, "square", "border", "border-black"];
+  return (
+    <div
+      data-testid="square"
+      className={classes.join(" ")}
+      onClick={() => setColor(color === COLOR_1 ? COLOR_2 : COLOR_1)}
+    >
+      {" "}
+    </div>
+  );
 }
--- src/tests/Grid/Square.test.js
+++ src/tests/Grid/Square.test.js
@@ -1,18 +1,30 @@
-import Square from "../../Components/Grid/Square"
-describe("Square should be a react component that represents a single unit in the grid/board", () => {
-    test("It should have 8 elements in an array", () => {
-        const Sq = <Square />
-        const squareProps = Sq.props;
-        expect(squareProps.adjacent.length).toBe(8);
-    });
-    test("It should initialise all adjacent squares to be null", () => {
-        const Sq = <Square />
-        const squareProps = Sq.props;
-        expect(squareProps.adjacent).toEqual(expect.arrayContaining([null,null,null,null,null,null,null,null]));
-    });
-    test("It should have state = visited:Bool", () => {
-        const Sq = <Square/>
-        //expect=(Sq.props)
-    })
+/**
+ * @jest-environment jsdom
+ */
 
-})
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Square from "../../Components/Grid/Square";
+
+describe("Square should be a react component that represents a single unit in the grid/board", () => {
+  test("It should change colour when clicked on from white to any other colour", () => {
+    render(<Square />);
+    const square = screen.getByTestId("square");
+    expect([...square.classList]).toEqual(expect.arrayContaining(["bg-white"]));
+    userEvent.click(square);
+    expect([...square.classList]).not.toEqual(
+      expect.arrayContaining(["bg-white"])
+    );
+  });
+  test("It should change colour when clicked from any colour to white", () => {
+    //styles cannot be computed outside of className, so ensure that square has border black 1px
+    render(<Square />);
+    const square = screen.getByTestId("square");
+    userEvent.click(square);
+    expect([...square.classList]).not.toEqual(
+      expect.arrayContaining(["bg-white"])
+    );
+    userEvent.click(square);
+    expect([...square.classList]).toEqual(expect.arrayContaining(["bg-white"]));
+  });
+});
