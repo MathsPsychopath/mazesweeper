a9059c7175deaa50a091c20a18385be0266796b9
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-02-14 00:47:03 +0000

    added gameReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import counterReducer from "./Counter/counter.reducer";
+import gameReducer from "./GameState/game.reducer";
 
 const rootReducer = combineReducers({
     counter: counterReducer,
+    game: gameReducer,
 });
 
 export default rootReducer;
-
