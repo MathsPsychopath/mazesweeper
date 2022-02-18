73ec63d62c8933f81bfc849b8bd637d51549de3f
Aaron Chan <0aaronchan@gmail.com>
2022-02-18 23:54:22 +0000

    changed SelectGrid to show 1 grid at a time

--- src/Components/Pages/SelectGrid/SelectGrid.js
+++ src/Components/Pages/SelectGrid/SelectGrid.js
@@ -5,21 +5,18 @@ import React from "react";
 
 const SIZES = ["10x10", "16x16", "16x30"];
 
-export default function SelectGrid() {
+export default function SelectGrid(props) {
+  const { gridSize } = props;
   return (
     <HeaderFooter>
       <div>
         <div>
+          <GridTypes
+            info={grids.types[gridSize]}
+            size={gridSize}
+            isDisplay={true}
+          />
           <h1>Select a grid size...</h1>
-          {SIZES.map((size) => (
-            <button key={size}>
-              <GridTypes
-                info={grids.types[size]}
-                size={size}
-                isDisplay={true}
-              />
-            </button>
-          ))}
         </div>
         <div>selection info</div>
       </div>
