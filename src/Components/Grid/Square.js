2b076d5832b602af93392dd6825af5667600d0d2
Aaron Chan <0aaronchan@gmail.com>
2022-02-26 21:54:53 +0000

    made squares more responsive, and used tcss vs css

--- src/Components/Grid/Square.js
+++ src/Components/Grid/Square.js
@@ -1,19 +1,23 @@
+import { useWindowWidth } from "@react-hook/window-size";
 import React from "react";
 import { useState } from "react";
-import "./square.css";
 
 const COLOR_1 = "bg-white";
 const COLOR_2 = "bg-orange-500";
 
 export default function Square(props) {
   const [color, setColor] = useState(COLOR_1);
+  const width = useWindowWidth();
+  const squareDim = props.isDisplay && width < 1536 ? "1.5em" : "2em";
   const classes = [
     color,
-    props.isDisplay ? "square-small" : "square",
     "hover:border-2",
+    `[min-width:${squareDim}] [max-width:${squareDim}] [min-height:${squareDim}] [max-height:${squareDim}] `,
     "hover:border-red-500",
     "border",
     "border-black",
+    "duration-50",
+    "cursor-pointer",
   ];
 
   if (props.isDisplay)
