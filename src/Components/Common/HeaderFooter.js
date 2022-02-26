69738855dc9ce9504dc62174f354eb3349f28a0e
Aaron Chan <0aaronchan@gmail.com>
2022-02-26 22:41:14 +0000

    changed gap-y to 10

--- src/Components/Common/HeaderFooter.js
+++ src/Components/Common/HeaderFooter.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function HeaderFooter(props) {
   return (
-    <div className="flex flex-col gap-y-32">
+    <div className="flex flex-col gap-y-10">
       <Header />
       {props.children}
       <Footer />
