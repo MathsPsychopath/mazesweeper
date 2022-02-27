ca74cdd781e304862b86f6a5f7e1297b9763ef9f
Aaron Chan <0aaronchan@gmail.com>
2022-02-27 21:30:39 +0000

    added initial PlayGame component

--- src/Components/Pages/PlayGame/PlayGame.js
+++ src/Components/Pages/PlayGame/PlayGame.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Grid from "../../Grid/Grid";
+import Timer from "../../Timer/Timer";
+import { useNavigate } from "react-router-dom";
 
 export default function PlayGame(props) {
-  const { time, paused, isInfinite, gameState } = props;
-
+  const { time, paused, gameState, mode, gridSize } = props;
+  const { changeMode, changeGrid, setInfinite, cycleGameState } = props;
+  const navigate = useNavigate();
+  if (time === 0) navigate("/results");
   return (
     <div className="mx-auto">
-      <h1>hello</h1>
+      <Grid />
+      <Timer {...props} />
     </div>
   );
 }
