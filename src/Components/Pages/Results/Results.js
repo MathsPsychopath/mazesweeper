441d0fc120ffeff0bf7f2318dc40ae6cde41598f
Aaron Chan <0aaronchan@gmail.com>
2022-03-15 23:39:23 +0000

    initial layout

---
+++ src/Components/Pages/Results/Results.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Results() {
+  return (
+    <div>
+      <h1>Results</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <div>
+        <button>Publish Stats</button>
+        <button>Play Again</button>
+      </div>
+    </div>
+  );
+}
