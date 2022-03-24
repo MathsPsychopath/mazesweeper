d00f405a11592da2bf337c208fa17bff5ff28ac2
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-24 20:30:59 +0000

    added styling and logic to Results

--- src/Components/Pages/Results/Results.js
+++ src/Components/Pages/Results/Results.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Publish from "./Publish";
 import Statistic from "./Statistic";
 import TableData from "./TableData";
+import { IoPlay } from "react-icons/io5";
 
 const stats = ["Average Time", "Grids Solved", "Size"];
 const pointMetrics = ["Base Score", "Penalties", "Time Bonuses", "Final Score"];
@@ -12,9 +14,9 @@ export default function Results() {
   const gameData = ["0:38", "5", "16x16"];
   return (
     //TODO #5 add user's historical data
-    <div className="mx-auto text-left w-9/12 mb-20 p-20 rounded-md">
+    <div className="mx-auto text-left w-9/12 p-20 rounded-md bg-gray-100 flex flex-col items-center">
       <h1 className="font-bold text-3xl">Results</h1>
-      <div className="flex flex-col md:flex-row p-4">
+      <div className="flex flex-col md:flex-row p-4 w-full">
         {stats.map((statistic, i) => (
           <div key={i} className="w-full mr-4">
             <Statistic statistic={statistic} gameData={gameData[i]} />
@@ -24,23 +26,33 @@ export default function Results() {
       <table className="table-fixed border-collapse w-full">
         <thead>
           <tr>
-            <th className="text-left font-normal text-gray-700 bg-gray-100 pl-4 rounded-l-md">
+            <th className="text-left font-normal text-gray-700 bg-white pl-4 rounded-l-md">
               Point Metrics
             </th>
-            <th className="text-left font-normal text-gray-700 bg-gray-100 pl-4 rounded-r-md">
+            <th className="text-left font-normal text-gray-700 bg-white pl-4 rounded-r-md">
               Points
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-300">
           {pointMetrics.map((metric, i) => (
-            <TableData metric={metric} metricData={pointsData[i]} />
+            <TableData
+              metric={metric}
+              metricData={pointsData[i]}
+              key={metric}
+            />
           ))}
         </tbody>
       </table>
-      <div>
-        <button onClick={() => navigate("/play")}>Play Again</button>
-        <button>Publish Stats</button>
+      <div className="w-full flex justify-center">
+        <button
+          className="flex items-center justify-center border-black rounded-md border bg-white p-4 w-1/3 m-2 hover:bg-blue-100"
+          onClick={() => navigate("/play")}
+        >
+          <IoPlay />
+          Play Again
+        </button>
+        <Publish />
       </div>
     </div>
   );
