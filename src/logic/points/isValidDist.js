c7ad2a3ea37d8f527c3e1e48fbfa9fd7191b06ce
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-26 23:59:55 +0000

    changed modename to use &

--- src/logic/points/isValidDist.js
+++ src/logic/points/isValidDist.js
@@ -1,19 +1,19 @@
 /**
- * 
- * @param {Number} distance - integer distance offset from correct answer 
+ *
+ * @param {Number} distance - integer distance offset from correct answer
  * @param {String} mode - either QuickMode | Normal | Chill and Casual
  * @returns true | false depending if the distance offset falls in error boundary of mode
  */
-export default function isValidDist(distance, mode){
-    const dist = Math.abs(distance);
-    switch(mode){
-        case "QuickMode":
-            return dist <= 1;
-        case "Normal":
-            return dist <= 2;
-        case "Chill and Casual":
-            return dist <= 3;
-        default:
-            return false;
-    }
+export default function isValidDist(distance, mode) {
+  const dist = Math.abs(distance);
+  switch (mode) {
+    case "QuickMode":
+      return dist <= 1;
+    case "Normal":
+      return dist <= 2;
+    case "Chill & Casual":
+      return dist <= 3;
+    default:
+      return false;
+  }
 }
--- src/tests/points/isValidDist.test.js
+++ src/tests/points/isValidDist.test.js
@@ -1,54 +1,58 @@
 import isValidDist from "../../logic/points/isValidDist";
 
 describe("isValidDist should return T/F based on distance from correct answer", () => {
-    test("it should return true when |distance| <= 1 if on QuickMode", () => {
-        const input = [
-            [0,"QuickMode"],     [1,"QuickMode"], 
-            [2,"QuickMode"],     [3,"QuickMode"], 
-            [9999, "QuickMode"], [-1, "QuickMode"],
-            [-5, "QuickMode"],   [-9999999, "QuickMode"]
-        ];
-        const output = [true, true, false, false, false, true, false, false];
-        for (let i = 0; i < 8; i++){
-            expect(isValidDist.apply(null, input[i])).toBe(output[i]);
-        }
-    });
-    test("it should return true when |distance| <= 2 if on Normal", () => {
-        const input = [
-            [0, "Normal"],   [1, "Normal"],
-            [2, "Normal"],   [3, "Normal"],
-            [999, "Normal"], [-1, "Normal"],
-            [-2, "Normal"],  [-3, "Normal"],
-        ];
-        const output = [true, true, true, false, false, true, true, false];
-        for (let i = 0; i < 8; i++){
-            expect(isValidDist.apply(null, input[i])).toBe(output[i]);
-        }
-    });
-    test("it should return true when |distance| <= 3 if on Chill and Casual - boundary", () => {
-        const input = [
-            [-3, "Chill and Casual"],
-            [-2, "Chill and Casual"],
-            [-1, "Chill and Casual"],
-            [0 , "Chill and Casual"],
-            [1 , "Chill and Casual"],
-            [2 , "Chill and Casual"],
-            [3 , "Chill and Casual"]
-        ];
-        for (let i = 0; i < 7; i++) expect(isValidDist.apply(null, input[i])).toBe(true);
-    });
-    test("it should return true when |distance| <= 3 if on Chill and Casual - erroneous", () => {
-        const input = [
-            [-4 , "Chill and Casual"],
-            [4  , "Chill and Casual"],
-            [50 , "Chill and Casual"],
-            [-50, "Chill and Casual"]
-        ];
-        input.forEach(args => expect(isValidDist.apply(null, args)).toBe(false));
-    });
-})
-
-
-
-
-
+  test("it should return true when |distance| <= 1 if on QuickMode", () => {
+    const input = [
+      [0, "QuickMode"],
+      [1, "QuickMode"],
+      [2, "QuickMode"],
+      [3, "QuickMode"],
+      [9999, "QuickMode"],
+      [-1, "QuickMode"],
+      [-5, "QuickMode"],
+      [-9999999, "QuickMode"],
+    ];
+    const output = [true, true, false, false, false, true, false, false];
+    for (let i = 0; i < 8; i++) {
+      expect(isValidDist.apply(null, input[i])).toBe(output[i]);
+    }
+  });
+  test("it should return true when |distance| <= 2 if on Normal", () => {
+    const input = [
+      [0, "Normal"],
+      [1, "Normal"],
+      [2, "Normal"],
+      [3, "Normal"],
+      [999, "Normal"],
+      [-1, "Normal"],
+      [-2, "Normal"],
+      [-3, "Normal"],
+    ];
+    const output = [true, true, true, false, false, true, true, false];
+    for (let i = 0; i < 8; i++) {
+      expect(isValidDist.apply(null, input[i])).toBe(output[i]);
+    }
+  });
+  test("it should return true when |distance| <= 3 if on Chill and Casual - boundary", () => {
+    const input = [
+      [-3, "Chill & Casual"],
+      [-2, "Chill & Casual"],
+      [-1, "Chill & Casual"],
+      [0, "Chill & Casual"],
+      [1, "Chill & Casual"],
+      [2, "Chill & Casual"],
+      [3, "Chill & Casual"],
+    ];
+    for (let i = 0; i < 7; i++)
+      expect(isValidDist.apply(null, input[i])).toBe(true);
+  });
+  test("it should return true when |distance| <= 3 if on Chill and Casual - erroneous", () => {
+    const input = [
+      [-4, "Chill & Casual"],
+      [4, "Chill & Casual"],
+      [50, "Chill & Casual"],
+      [-50, "Chill & Casual"],
+    ];
+    input.forEach((args) => expect(isValidDist.apply(null, args)).toBe(false));
+  });
+});
