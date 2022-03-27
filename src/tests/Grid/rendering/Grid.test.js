c9d3a9ffd9c7c80e96dc805f5056ce436e4fd55c
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-27 23:25:13 +0100

    fixed failed tests

--- src/tests/Grid/rendering/Grid.test.js
+++ src/tests/Grid/rendering/Grid.test.js
@@ -4,7 +4,7 @@ import Grid from "../../../Components/Grid/Grid";
 describe("Grid should be a react component that is a container to many Square components", () => {
   //size is the number of Square components, in dimension form or raw value
   test("It should have a default size of 10x10", () => {
-    render(<Grid />);
+    render(<Grid isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(10);
     grid.childNodes.forEach((row) => {
@@ -13,7 +13,7 @@ describe("Grid should be a react component that is a container to many Square co
   });
 
   test("It should accept 16x16 grid", () => {
-    render(<Grid gridSize="16x16" />);
+    render(<Grid gridSize="16x16" isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(16);
     grid.childNodes.forEach((row) => {
@@ -22,7 +22,7 @@ describe("Grid should be a react component that is a container to many Square co
   });
 
   test("It should accept 16x30 grid", () => {
-    render(<Grid gridSize="16x30" />);
+    render(<Grid gridSize="16x30" isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(16);
     grid.childNodes.forEach((row) => {
@@ -31,7 +31,7 @@ describe("Grid should be a react component that is a container to many Square co
   });
 
   test("It should replace a 0x0 grid with 10x10", () => {
-    render(<Grid gridSize="0x0" />);
+    render(<Grid gridSize="0x0" isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(10);
     grid.childNodes.forEach((row) => {
@@ -40,7 +40,7 @@ describe("Grid should be a react component that is a container to many Square co
   });
 
   test("It should replace a 11x11 grid with 10x10", () => {
-    render(<Grid gridSize="11x11" />);
+    render(<Grid gridSize="11x11" isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(10);
     grid.childNodes.forEach((row) => {
@@ -49,7 +49,7 @@ describe("Grid should be a react component that is a container to many Square co
   });
 
   test("It should replace a 9999999x9999999 grid with 10x10", () => {
-    render(<Grid gridSize="9999999x9999999" />);
+    render(<Grid gridSize="9999999x9999999" isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(10);
     grid.childNodes.forEach((row) => {
@@ -58,7 +58,7 @@ describe("Grid should be a react component that is a container to many Square co
   });
 
   test("It should replace a -1x-1 grid with 10x10", () => {
-    render(<Grid gridSize="-1x-1" />);
+    render(<Grid gridSize="-1x-1" isDisplay />);
     const grid = screen.getByTestId("grid");
     expect(grid.childElementCount).toEqual(10);
     grid.childNodes.forEach((row) => {
--- src/tests/Grid/rendering/Row.test.js
+++ src/tests/Grid/rendering/Row.test.js
@@ -22,43 +22,78 @@ export const testIfOnlySquares = (row) => {
 
 describe("Row should be a react component that contains a number of Square Components", () => {
   test("It should have a default of 10 columns", () => {
-    render(<Row />);
+    render(<Row isDisplay />);
     const row = screen.getByTestId("row");
     expect(row.childElementCount).toEqual(10);
   });
 
   test("It should have only Square Components", () => {
-    render(<Row />);
+    render(
+      <Row
+        reveal={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        row={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+      />
+    );
     const row = screen.getByTestId("row");
     testIfOnlySquares(row);
   });
 
   test("It should also accept 16 column length", () => {
-    render(<Row columns={16} />);
+    render(
+      <Row
+        columns={16}
+        reveal={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        row={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+      />
+    );
     const row = screen.getByTestId("row");
     expect(testIfOnlySquares(row)).toEqual(16);
   });
 
   test("It should accept a 30 column length", () => {
-    render(<Row columns={30} />);
+    render(
+      <Row
+        columns={30}
+        reveal={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        row={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+      />
+    );
     const row = screen.getByTestId("row");
     expect(testIfOnlySquares(row)).toEqual(30);
   });
 
   test("It should have 10 columns for every other column length argument: 0", () => {
-    render(<Row columns={0} />);
+    render(
+      <Row
+        columns={0}
+        reveal={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        row={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+      />
+    );
     const row = screen.getByTestId("row");
     expect(testIfOnlySquares(row)).toEqual(10);
   });
 
   test("It should have 10 columns for every other column length argument: -1", () => {
-    render(<Row columns={-1} />);
+    render(
+      <Row
+        columns={-1}
+        reveal={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        row={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+      />
+    );
     const row = screen.getByTestId("row");
     expect(testIfOnlySquares(row)).toEqual(10);
   });
 
   test("It should have 10 columns for every other column length argument: 9999", () => {
-    render(<Row columns={9999} />);
+    render(
+      <Row
+        columns={9999}
+        reveal={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        row={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+      />
+    );
     const row = screen.getByTestId("row");
     expect(testIfOnlySquares(row)).toEqual(10);
   });
