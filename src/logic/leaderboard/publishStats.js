81c8fc38515b5c06358239da22071aa67069d09b
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-27 21:29:41 +0100

    added most i can to publish Stats

--- src/logic/leaderboard/publishStats.js
+++ src/logic/leaderboard/publishStats.js
@@ -4,17 +4,20 @@ import {
   setUnpublished,
 } from "../../redux/MenuSelection/menu.actions";
 
-const URL = ""; //TODO replace with EC2 Endpoint
+const URL = ""; //TODO replace with GCP CF Endpoint
 
 export default async function publishStats(dispatch, gameData) {
   dispatch(setPublishing());
   const data = Object.keys(gameData)
-    .filter((key) => key !== "gameState" && key !== "gridState")
+    .filter(
+      (key) =>
+        !isNaN(Number(gameData[key].length === 0 ? undefined : gameData[key]))
+    )
     .reduce((acc, key) => {
       acc[key] = gameData[key];
       return acc;
     }, {});
-
+  console.log(data);
   await fetch(URL, {
     method: "POST",
     mode: "cors",
