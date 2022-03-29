b41066b10f5f08bb07e7b5085c2184b074edb0b9
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-29 20:43:20 +0100

    got rid of unn. comp. + styled

--- src/Components/Pages/Leaderboard/Leaderboard.js
+++ src/Components/Pages/Leaderboard/Leaderboard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Statistic from "../Results/Statistic";
+import Statistic from "../../Common/Statistic";
 import SearchParameters from "./SearchParameters";
 import SuspenseStatistic from "./SuspenseStatistic";
 
@@ -7,8 +7,9 @@ function StatisticBlob({ head, subhead }) {
   return (
     <div
       className={
-        "py-2 px-4 bg-yellow-400 text-white font-semibold rounded-lg " +
-        "flex flex-col items-center border-orange-400 border-l-4 border-b-4"
+        "py-2 px-4 bg-purple-200 text-white font-semibold rounded-lg " +
+        "flex flex-col items-center border-purple-400 border-l-4 border-b-4"
+        //potential alt. cyan, blue, green, @400/600, @200/400
       }
     >
       <h1>{head}</h1>
@@ -17,35 +18,28 @@ function StatisticBlob({ head, subhead }) {
   );
 }
 
-function StatsList({ data }) {
+function Stats({ i, dataObject }) {
+  if (!dataObject.gameStats || !dataObject.username)
+    return <SuspenseStatistic />;
   return (
-    <>
-      {data.map((dataObject, i) => {
-        if (dataObject)
-          return (
-            <Statistic key={i}>
-              <div className="flex flex-col grow sm:flex-row gap-1 justify-evenly ">
-                {Object.entries(dataObject.gameStats).map((stat) => (
-                  <StatisticBlob head={stat[1]} subhead={stat[0]} />
-                ))}
-              </div>
-              <h1 className="sm:pl-8 text-gray-400 text-xl">
-                {i + 1}:{dataObject.username}
-              </h1>
-            </Statistic>
-          );
-        return (
-          <div key={i} className="flex flex-row grow sm:flex-col">
-            <SuspenseStatistic />
-          </div>
-        );
-      })}
-    </>
+    <div className="">
+      <Statistic>
+        <div className="flex flex-col grow sm:flex-row md:gap-1 justify-evenly ">
+          {Object.entries(dataObject.gameStats).map((stat) => (
+            <StatisticBlob head={stat[1]} subhead={stat[0]} />
+          ))}
+          <h1 className="sm:pl-8 text-gray-400 text-xl">
+            {i + 1}:{dataObject.username}
+          </h1>
+        </div>
+      </Statistic>
+    </div>
   );
 }
 
 export default function Leaderboard() {
-  //const [data, setData] = useState([...Array(5)]);
+  //const [data, setData] = useState([{}, {}, {}, {}]);
+
   const [data, setData] = useState([
     {
       gameStats: {
@@ -102,8 +96,15 @@ export default function Leaderboard() {
     <div className="mx-auto text-left py-8 px-auto rounded-md bg-white flex flex-col items-center">
       <h1 className="font-bold text-3xl my-2">Leaderboard</h1>
       <SearchParameters setData={setData} />
-      <div className="w-full md:w-[40em] flex flex-wrap sm:flex-col gap-2 sm:gap-4">
-        <StatsList data={data} />
+      <div className="m-2 w-full md:w-[40em] flex flex-wrap sm:flex-col gap-2 sm:gap-4">
+        {data.map((dataObject, i) => (
+          <div
+            className="flex flex-row grow sm:flex-col justify-center"
+            key={i}
+          >
+            <Stats i={i} dataObject={dataObject} />
+          </div>
+        ))}
       </div>
     </div>
   );
