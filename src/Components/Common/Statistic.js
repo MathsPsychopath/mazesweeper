d5fd3d377f84e70de8b684d105558567553fd323
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-29 20:36:36 +0100

    moved statistics to common

--- src/Components/Common/Statistic.js
+++ src/Components/Common/Statistic.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 export default function Statistic({ gameData, statistic, children }) {
+  const renderResults = gameData !== undefined && statistic !== undefined;
   return (
     <div
       className={
-        "rounded-md w-full flex p-4 border-purple-500 border-b-4 " +
+        "rounded-md w-full flex p-2 md:p-4 border-purple-500 border-b-4 " +
         "border-l-4 bg-white flex flex-col bg-gray-50"
       }
     >
-      {gameData && statistic ? (
+      {renderResults ? (
         <>
           <h1>{gameData}</h1>
           <p className="text-gray-400">{statistic}</p>
