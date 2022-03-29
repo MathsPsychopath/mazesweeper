623e2a223b997d1c802481b39a643eb81cc4a852
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-29 01:55:44 +0100

    added textentry and flexcol'd OptionsList

--- src/Components/Common/OptionsList.js
+++ src/Components/Common/OptionsList.js
@@ -3,18 +3,20 @@ import OptionButton from "./OptionButton";
 
 export default function OptionsList({ setState, state, updateValues, title }) {
   return (
-    <div className="flex gap-x-2">
-      <h1 className="text-xl align-bottom">{title}</h1>
-      {updateValues.map((value) => (
-        <OptionButton
-          currentState={state === value}
-          updateState={setState}
-          updateValue={value}
-          key={value}
-        >
-          {value}
-        </OptionButton>
-      ))}
+    <div className="flex flex-col">
+      <h1 className="text-xl">{title}</h1>
+      <div className="flex gap-x-2">
+        {updateValues.map((value) => (
+          <OptionButton
+            currentState={state === value}
+            updateState={setState}
+            updateValue={value}
+            key={value}
+          >
+            {value}
+          </OptionButton>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Common/TextEntry.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TextEntry({ input, setInput, isDisabled }) {
+  function handleChange(event) {
+    return event.target.validity.valid ? event.target.value : input;
+  }
+
+  return (
+    <div className="border-2 border-black rounded-sm w-60">
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(handleChange(event))}
+        className="w-full"
+        disabled={isDisabled}
+      />
+    </div>
+  );
+}
